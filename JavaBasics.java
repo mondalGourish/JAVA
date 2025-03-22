fef72082ec306e9a31b45de9c52c7716d0eb9d09
Gourish Mondal <gourishmondal02@gmail.com>
2025-03-22 13:28:59 +0530

    Initial commit

---
+++ JavaBasics.java
@@ -0,0 +1,8 @@
+public class JavaBasics{
+    public static void main(String args[]){
+        System.out.println("1 2 3!"); 
+        System.out.println("1 2 3 4 5!"); 
+
+    }
+}
+//boilerplate code
