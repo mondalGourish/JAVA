26229724d03597315b854dd72531118bfb2c9b64
mondalGourish <gourishmondal02@gmail.com>
2026-03-11 22:14:05 +0530

    practice questions

--- practice_new/solution.java
+++ practice_new/solution.java
@@ -214,7 +214,7 @@
 //   }
 // }
 
-//find ap
+//find ap and sum
 // import java.util.Scanner;
 // public class solution{
 //   public static void apSeries(int n, int a, int d){
@@ -232,5 +232,177 @@
 //     int a = sc.nextInt();
 //     int d = sc.nextInt();
 //     apSeries(n,a,d);
+//   }
+// }
+
+//find gp and sum
+// import java.util.Scanner;
+// public class solution{
+//   public static void gpSeries(int a, float r, int n){
+//     float sum = 0;
+//     float term = a;
+//     for(int i=0; i<n; i++){
+//       System.out.print(term+" ");
+//       sum = sum+term;
+//       term = term*r;
+//     }
+//     System.out.println();
+//     System.out.print(sum);
+//   }
+//   public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     int a = sc.nextInt();
+//     float r = sc.nextFloat();
+//     int n = sc.nextInt();
+//     gpSeries(a,r,n);
+//   }
+// }
+
+//greater among two
+// import java.util.Scanner;
+// public class solution{
+//   public static void greaterTwo(double n, double m){
+//     if(n>m){
+//       System.out.print(n);
+//     }else{
+//       System.out.print(m);
+//     }
+//   }
+//   public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     double n = sc.nextDouble();
+//     double m = sc.nextDouble();
+//     greaterTwo(n,m);
+//   }
+// }
+
+//greater among 3
+// import java.util.Scanner;
+// public class solution{
+//   public static void greaterThree(double n, double m, double p){
+//     if(n>m && n>p){
+//       System.out.print(n);
+//     }else if(m>p){
+//       System.out.print(m);
+//     }else{
+//       System.out.print(p);
+//     }
+//   }
+//   public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     double n = sc.nextDouble();
+//     double m = sc.nextDouble();
+//     double p = sc.nextDouble();
+//     greaterThree(n,m,p);
+//   }
+// }
+
+//leap year
+// import java.util.Scanner;
+// public class solution{
+//   public static void leapYear(int n){
+//     if((n%4==0)&&(n%100==0) || (n%400 == 0)){
+//       System.out.print("yes");
+//     }else{
+//       System.out.print("no");
+//     }
+//   }
+  
+//   public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     leapYear(n);
+//   }
+// }
+
+//reverse
+// import java.util.Scanner;
+// public class solution{
+//   public static void reverse(int n){
+//     int rev = 0;
+//     while(n!=0){
+//       int digit = n%10;
+//       rev = rev*10+digit;
+//       n /= 10;
+//     }
+//     System.out.print(rev);
+//   }
+//   public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     reverse(n);
+//   }
+// }
+
+//maximum and minimum digit in a number
+// import java.util.Scanner;
+// public class solution{
+//   public static void smallerGreater(int n){
+//     int minDigit = 9;
+//     int maxDigit = 0;
+//     while(n != 0){
+//       int digit = n%10;
+//       if(digit>maxDigit){
+//         maxDigit = digit;
+//       }
+//       if(digit<minDigit){
+//         minDigit = digit;
+//       }
+      
+//       n/=10;
+//     }
+//     System.out.print("MaxDigit:"+maxDigit+" ");
+//     System.out.print("MinDigit:"+minDigit);
+//   }
+//   public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     smallerGreater(n);
+//   }
+// }
+
+//fibonacci
+// import java.util.Scanner;
+// public class solution{
+//   public static void fibonacci(int n){
+//     if(n == 0){
+//       System.out.print("The fibonacci series upto "+n+" is:");
+//       System.out.println(0);
+//     }else{
+//       int secLast = 0;
+//       int last = 1;
+//       System.out.println("The fibonacci series upto "+n+" is:");
+//       System.out.print(secLast+" "+last+" ");
+      
+//       int curr;
+//       for(int i=2; i<=n; i++){
+//         curr = secLast+last;
+//         secLast = last;
+//         last = curr;
+//         System.out.print(curr+" ");
+//       }
+//     }
+//   }
+//   public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     fibonacci(n);
+//   }
+// }
+
+//factorial
+// import java.util.Scanner;
+// public class solution{
+//   public static void factorial(int n){
+//     int fact = 1;
+//     for(int i=1; i<=n; i++){
+//       fact *= i;
+//     }
+//     System.out.print(fact);
+//   }
+//   public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     factorial(n);
 //   }
 // }
